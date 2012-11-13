0a17c8156e1d45322762d0788fdd81401f821d61
MarkPent <mark.pent@gmail.com>
2012-11-13 21:08:41 +1000

    set meta in test worker

--- spec/resources/summary_worker.rb
+++ spec/resources/summary_worker.rb
@@ -6,6 +6,7 @@ class SummaryWorker < BackgroundQueue::Worker::Base
         raise "Invalid Summary (#{summary.inspect})" if summary[:test] != [1, 2] && summary[:test] != [2, 1]
       else
         raise "Missing TestId" if params[:test_id].nil?
+        add_progress_meta(:meta, {:test_meta=>"something"})
         append_summary(:test, params[:test_id])
       end
       set_progress("Done", 100)
